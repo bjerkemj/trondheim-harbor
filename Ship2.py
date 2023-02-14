70ed87dc0a15a4d647af1c798d7bde3014594bed
@tinusfa <tinusfa@stud.ntnu.no>
2023-02-14 12:21:32 +0100

    ?

--- Ship2.py
+++ Ship2.py
@@ -37,7 +37,7 @@ class Ship2:
 
     def isFull(self) -> bool:
         return len(self.freeSections) == 0
-    
+
     def getSection(self, sectionId) -> List[ShipSection]:
         for section in self.freeSections:
             if section.getSectionId() == sectionId:
@@ -46,7 +46,7 @@ class Ship2:
             if section.getSectionId() == sectionId:
                 return section
 
-    def updateShipWeight(self) -> None: # Johan mener denne kan slettes?
+    def updateShipWeight(self) -> None:  # Johan mener denne kan slettes?
         pass
 
     def isShipBalanced(self) -> bool:
@@ -92,21 +92,21 @@ class Ship2:
         for section in allSections:
             totalWeight += section.getSectionWeight()
         return totalWeight
-    
+
     def getTotalWeightStarboard(self) -> int:
         totalWeightStarboard = 0
         allSections = self.getAllSections()
         for section in allSections:
-           if section.getSectionId() in [1,3,5]:
-               totalWeightStarboard += section.getSectionWeight()
+            if section.getSectionId() in [1, 3, 5]:
+                totalWeightStarboard += section.getSectionWeight()
         return totalWeightStarboard
-               
+
     def getTotalWeightPort(self) -> int:
         totalWeightPort = 0
         allSections = self.getAllSections()
         for section in allSections:
-           if section.getSectionId() in [0,2,4]:
-               totalWeightPort += section.getSectionWeight()
+            if section.getSectionId() in [0, 2, 4]:
+                totalWeightPort += section.getSectionWeight()
         return totalWeightPort
 
     def getTotalWeightSections(self) -> List[int]:
@@ -114,14 +114,14 @@ class Ship2:
         allSections = self.getAllSections()
         for section in allSections:
             sectionId = section.getSectionId()
-            if sectionId in [0,1]:
+            if sectionId in [0, 1]:
                 sectionWeights[0] += section.getSectionWeight()
-            elif sectionId in [2,3]:
+            elif sectionId in [2, 3]:
                 sectionWeights[1] += section.getSectionWeight()
-            elif sectionId in [4,5]:
+            elif sectionId in [4, 5]:
                 sectionWeights[2] += section.getSectionWeight()
         return sectionWeights
-    
+
     def isShipBalanced(self, x_perc=0.05, y_perc=0.1):
         weightPortside = self.getTotalWeightPort()
         weightStarboard = self.getTotalWeightStarboard()
@@ -134,7 +134,7 @@ class Ship2:
         if weightPortside < weightStarboard * (1 - x_perc):
             print("Starboard to heavy")
             return False
-        
+
         if weightSection[1] > weightSection[0] * (1 + y_perc) or weightSection[2] > weightSection[0] * (1 + y_perc):
             print("Mid or stern section to heavy")
             return False
@@ -145,29 +145,29 @@ class Ship2:
 
         if weightSection[0] > weightSection[2] * (1 + y_perc) or weightSection[1] > weightSection[2] * (1 + y_perc):
             print("Bow or mid section to heavy")
-            return False 
+            return False
 
         print("The ship is loaded correctly")
         return True
-    
+
     def saveToFile(self, filename="shipSave"):
-            with open(os.path.join(ROOT, filename + ".tsv"), "w") as f:
-                allSections = self.getAllSections()
-                for section in allSections:
-                    f.write("Section: " + str(section.getSectionId()) + "\n")
-                    width = section.getWidth()
-                    length = section.getLength()
-                    for l in range(length):
-                        for w in range(width):
-                            for containers in section.getStack((w,l)).getContainers():
-                                for container in containers:
-                                    f.write(container.getId() + "\t")
-                                    f.write(str(container.getSize()) + "\t")
-                                    f.write(str(container.getWeight()) + "\t")
-                                    f.write(str(container.getCapacity()) + "\t")
-                                    f.write(str(container.getLoad()) + "\t")
-                                    f.write("-\t")
-                            f.write("\n")
+        with open(os.path.join(ROOT, filename + ".tsv"), "w") as f:
+            allSections = self.getAllSections()
+            for section in allSections:
+                f.write("Section: " + str(section.getSectionId()) + "\n")
+                width = section.getWidth()
+                length = section.getLength()
+                for l in range(length):
+                    for w in range(width):
+                        for containers in section.getStack((w, l)).getContainers():
+                            for container in containers:
+                                f.write(container.getId() + "\t")
+                                f.write(str(container.getSize()) + "\t")
+                                f.write(str(container.getWeight()) + "\t")
+                                f.write(str(container.getCapacity()) + "\t")
+                                f.write(str(container.getLoad()) + "\t")
+                                f.write("-\t")
+                        f.write("\n")
 
     def readFromFile(self, filename="shipSave"):
         with open(os.path.join(ROOT, filename + ".tsv"), "r") as f:
@@ -186,27 +186,32 @@ class Ship2:
                 rowInfo = line.split("-")
                 for info in rowInfo:
                     infoSplitted = info.strip("\n").strip("\t").split("\t")
-                    if(len(infoSplitted)==1):
+                    if(len(infoSplitted) == 1):
                         continue
-                    container = Container(int(infoSplitted[1]), infoSplitted[0], int(infoSplitted[4]))
-                    if container.getSize==40:
+                    container = Container(
+                        int(infoSplitted[1]), infoSplitted[0], int(infoSplitted[4]))
+                    if container.getSize == 40:
                         stack.pushContainer([container])
-                    elif container.getSize==20 and listFor20Containers:
+                    elif container.getSize == 20 and listFor20Containers:
                         listFor20Containers.append(container)
                         stack.pushContainer(listFor20Containers)
                         listFor20Containers = []
                     else:
                         listFor20Containers.append(container)
-                widthCounter+=1
+                widthCounter += 1
                 if widthCounter == width:
-                    widthCounter=0
-                    lengthCounter+=1
+                    widthCounter = 0
+                    lengthCounter += 1
+
 
 def main():
 
     ship = Ship2()
     numContainers = 20000
+    start = time.time()
+
     randomContainers = createRandomContainers(numContainers)
+    print('Tid = ', time.time() - start)
     numContainersAdded = 0
     start = time.time()
     k = []
@@ -226,7 +231,7 @@ def main():
         print(f'Script took {end - start:0f} seconds')
         print(
             f"Number of crane operations using a single crane was: {ship.getNumberOfOperationsInShip()}")
-    
+
     print(len(ship.getAllSections()))
     print(ship.getTotalWeight())
     print(ship.getTotalWeightPort())
@@ -238,8 +243,6 @@ def main():
     ship.saveToFile()
     ship.readFromFile()
     ship.saveToFile("shipSave2")
-
-
 
 
 if __name__ == '__main__':
