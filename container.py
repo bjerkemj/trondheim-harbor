6535d8408ff7d10bb026ec94ce051b7327b80cf6
Johan Bjerkem <bjerkem.j@gmail.com>
2023-02-14 13:36:40 +0100

    container tests

--- container.py
+++ container.py
@@ -105,29 +105,52 @@ def createContainerId():
 
 if __name__ == '__main__':
     # Single container test
-    print("Single container test")
+    # ---------------------
     c1 = Container(20, "ID1", 5)
-    print("ID1 = " + c1.getId())
-    print("20 = " + str(c1.getCapacity()))
-    print("5 = " + str(c1.getLoad()))
-    print("7 = " + str(c1.getTotalWeight()))
-    print("2 = " + str(c1.getWeight()))
-    c1.print()
-    c1.setLoad(20)
-    print("20 = " + str(c1.getLoad()))
-    print("22 = " + str(c1.getTotalWeight()))
-    c2 = Container(40, "ID2", 7)
-    c2.print()
+    assert c1.getId() == "ID1", \
+        f"Container id  should be ID1 but was {c1.getId()}"
+    assert c1.getCapacity() == 20, \
+        f"Container capacity should be 20 but was {c1.getCapacity()}"
+    assert c1.getLoad() == 5, \
+        f"Container load should be 5 but was {c1.getLoad()}"
+    assert c1.getTotalWeight() == 7, \
+        f"Total container weight weight should be 7 but was {c1.getTotalWeight()}"
+    assert c1.getWeight() == 2, \
+        f"Container inherit weight should be 2 but was {c1.getWeight()}"
 
-    print()
+    c1.setLoad(20)
+    assert c1.getLoad() == 20, \
+        f"Container load should be 20 but was {c1.getLoad()}"
+    assert c1.getTotalWeight() == 22, \
+        f"Total container weight weight should be 22 but was {c1.getTotalWeight()}"
+
+    c2 = Container(40, "ID2", 7)
+    assert c2.getId() == "ID2", \
+        f"Container id  should be ID2 but was {c2.getId()}"
+    assert c2.getCapacity() ==  22, \
+        f"Container capacity should be 22 but was {c2.getCapacity()}"
+    assert c2.getLoad() == 7, \
+        f"Container load should be 7 but was {c2.getLoad()}"
+    assert c2.getTotalWeight() == 11, \
+        f"Total container weight weight should be 11 but was {c2.getTotalWeight()}"
+    assert c2.getWeight() == 4, \
+        f"Container inherit weight should be 4 but was {c2.getWeight()}"
 
     # Random container test
+    # ---------------------
+    
     random.seed(1)
     print("Random container test")
-    r1 = createRandomContainer()
-    r1.print()
-    r1.setLoad(10)
-    print("10 = " + str(r1.getLoad()))
-    randomContainer = createRandomContainers(5)
-    for container in randomContainer:
-        container.print()
+    rc1 = createRandomContainer()
+    assert isinstance(rc1, Container), \
+        f"The object should be a container but was {type(c1).__name__}"
+    rc1.setLoad(10)
+    assert rc1.getLoad() == 10, \
+        f"Container load should be 7 but was {rc1.getLoad()}"
+    
+    randomContainers = createRandomContainers(5)
+    for container in randomContainers:
+        assert isinstance(container, Container), \
+            f"The object should be a container but was {type(container).__name__}"
+
+
